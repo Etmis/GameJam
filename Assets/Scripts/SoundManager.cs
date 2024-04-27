c91177ed077611688a6797782a622dfd5d29a7e1
FrantaGergi <panda.gergel@gmail.com>
2024-04-27 20:41:19 +0200

    Towers
    BlouWobs

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance; 
+
+    [SerializeField]
+    private AudioSource audioSource; 
+
+   
+    [System.Serializable]
+    private class Sound
+    {
+        public string name; 
+        public AudioClip clip; 
+    }
+
+    [SerializeField]
+    private Sound[] sounds; 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void PlaySound(string name)
+    {
+        if (instance != null)
+        {
+            Sound sound = System.Array.Find(instance.sounds, s => s.name == name); 
+
+            if (sound != null && instance.audioSource != null)
+            {
+                instance.audioSource.PlayOneShot(sound.clip);
+            }
+            else
+            {
+                Debug.LogWarning("Zvuk " + name + " nebyl nalezen nebo nebyl nastaven AudioSource.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("SoundManager instance není inicializována.");
+        }
+    }
+}
